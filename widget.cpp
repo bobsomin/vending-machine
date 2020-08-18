696fa4f03749a02c3bae872ac69579c5a9519d4f
bobsomin <68039471+bobsomin@users.noreply.github.com>
2020-08-19 03:17:15 +0900

    Update widget.cpp

--- widget.cpp
+++ widget.cpp
@@ -2,6 +2,7 @@
 #include "ui_widget.h"
 #include <QMessageBox>
 #include <sstream>
+#include <QPixmap>
 
 using namespace std;
 
@@ -9,13 +10,17 @@ Widget::Widget(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::Widget) {
     ui->setupUi(this);
+    QPixmap pix("/home/somin/magic-vending-machine/genie.jpg");
+    ui->label_pic->setPixmap(pix.scaled(350, 250, Qt::KeepAspectRatio));
+    ui->RandomButton->setEnabled(false);
 }
 
 Widget::~Widget() {
     delete ui;
 }
 
-void Widget::change_money(int a) {
+
+void Widget::exchange(int a) {
     money += a;
     deactivate_buttons();
     ui->lcdNumber->display(money);
@@ -26,36 +31,39 @@ void Widget::deactivate_buttons() {
     else ui->pb_coffee->setEnabled(true);
     if (money < 150) ui->pb_tea->setEnabled(false);
     else ui->pb_tea->setEnabled(true);
+    if (money < 200) ui->RandomButton->setEnabled(false);
+    else ui->RandomButton->setEnabled(true);
     if (money < 300) ui->pb_coke->setEnabled(false);
     else ui->pb_coke->setEnabled(true);
+
 }
 
 void Widget::on_pb10_clicked(){
-    change_money(10);
+    exchange(10);
 }
 
 void Widget::on_pb50_clicked(){
-    change_money(50);
+    exchange(50);
 }
 
 void Widget::on_pb100_clicked(){
-    change_money(100);
+    exchange(100);
 }
 
 void Widget::on_pb500_clicked(){
-    change_money(500);
+    exchange(500);
 }
 
 void Widget::on_pb_coffee_clicked(){
-    change_money(-100);
+    exchange(-100);
 }
 
 void Widget::on_pb_tea_clicked(){
-    change_money(-150);
+    exchange(-150);
 }
 
 void Widget::on_pb_coke_clicked(){
-    change_money(-300);
+    exchange(-300);
 }
 
 void Widget::on_pb_return_clicked(){
@@ -68,9 +76,24 @@ void Widget::on_pb_return_clicked(){
     for (int i = 0; i < 4; ++i) {
         count[i] = money / measure[i];
         money -= count[i] * measure[i];
-        content << measure[i] << "돈 " << count[i] << endl;
-    }
 
-    change_money(-money);
-    msg.information(nullptr, "거슬러줌", content.str().c_str());
+        content << measure[i] << "₩  " << count[i] << "개" << endl ;
+    }
+    exchange(-money);
+    msg.information(nullptr, "안 가져가면 다음사람이 가져감", content.str().c_str());
+}
+
+void Widget::on_pushButton_clicked()
+{
+
+}
+
+void Widget::on_RandomButton_clicked()
+{
+    QMessageBox msgBox;
+    exchange(-200);
+    msgBox.setText("꽝");
+    msgBox.exec();
+
+
 }
